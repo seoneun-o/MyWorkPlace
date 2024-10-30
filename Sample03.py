30eaad3ed5c38f66de797de048fc6f00b7abb21f
Seoneun Oh <seoneun12@gmail.com>
2024-10-31 08:23:31 +0900

    odd number

---
+++ Sample03.py
@@ -0,0 +1,13 @@
+# python for, if, try
+# print out all even numbers between 1 and 10
+# for each in range(1,11,1):
+#     if(each % 2 == 0):
+#         print(each)
+
+
+# print out all odd numbers between 1 and 100
+# show your work below...
+for each in range(1,11,1):
+    if(each % 2 == 1):
+        print(each)
+        
